6b7a9d99f89b3ce2712afa266f1639d1a4e895d8
서지상 <inaki96@naver.com>
2021-07-13 15:52:21 +0900

    rendering mistake fin, fin red cube

--- c1/script.js
+++ c1/script.js
@@ -1,29 +1,38 @@
 //Scene
-const scene = new THREE.Scene();
 
-//Red Cube
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({
-	color: '#ff0000'
-});
-const mesh = new THREE.Mesh(geometry, material);
-scene.add(mesh);
+window.onload = () => {
+	const scene = new THREE.Scene();
 
-//Camera
-const sizes = {
-	width: 800,
-	height: 600
+	//Red Cube
+	const geometry = new THREE.BoxGeometry(1, 1, 1);
+	const material = new THREE.MeshBasicMaterial({
+		color: 0xff0000
+	});
+
+	const mesh = new THREE.Mesh(geometry, material);
+	scene.add(mesh);
+
+	//Camera
+	const sizes = {
+		width: 800,
+		height: 600
+	};
+	// 시야각 = fov 망원경 vs 전체 , aspect Ratio  width / height of viewPort
+	const camera = new THREE.PerspectiveCamera(60, sizes.width / sizes.height);
+	camera.position.z = 3;
+
+	scene.add(camera);
+
+	//renderer
+	//render scene from the camera point
+	const canvas = document.querySelector('.webgl');
+	const renderer = new THREE.WebGLRenderer({
+		canvas: canvas
+	});
+
+	// renderer의 사이즈를 정함
+
+	renderer.setSize(sizes.width, sizes.height);
+
+	renderer.render(scene, camera);
 };
-// 시야각 = fov 망원경 vs 전체 , aspect Ratio  width / height of viewPort
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
-scene.add(camera);
-
-//renderer
-//render scene from the camera point
-
-const renderer = new THREE.WebGLRenderer({
-	canvas: document.querySelector('canvas')
-});
-
-// renderer의 사이즈를 정함
-renderer.setSize(sizes.width, sizes.height);
