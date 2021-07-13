c4a0fd976aab75de15764f92f17a083b64eedd1f
서지상 <inaki96@naver.com>
2021-07-13 16:56:01 +0900

    group learned

--- src/script.js
+++ src/script.js
@@ -3,28 +3,25 @@ import * as THREE from 'three';
 
 const scene = new THREE.Scene();
 
-//Red Cube
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({
-	color: 0xff0000
-});
+//object
 
-const mesh = new THREE.Mesh(geometry, material);
-mesh.position.x = 0.7;
-mesh.position.y = -0.6;
-mesh.position.z = 2;
-// ===> mesh.position.set(0.7, -.6, 2);
-//inherited from Vector3
+const group = new THREE.Group();
+scene.add(group);
 
-//ordering rotation order
-mesh.rotation.reorder('YXZ');
-mesh.rotation.x = Math.PI * 0.25;
-mesh.rotation.y = Math.PI * 0.25;
+const cube1 = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1), new THREE.MeshBasicMaterial({ color: 0xff0000 }));
 
-mesh.scale.set(2, 2, 2);
+const cube2 = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1), new THREE.MeshBasicMaterial({ color: 0x00ff00 }));
 
-scene.add(mesh);
+cube2.position.set(1, 0, -2);
 
+group.add(cube1);
+group.add(cube2);
+
+group.position.set(0, 1, 0);
+group.scale.y = 2;
+group.rotation.y = 1;
+
+scene.add(group);
 const axesHelper = new THREE.AxesHelper();
 scene.add(axesHelper);
 
@@ -33,23 +30,18 @@ const sizes = {
 	width: 800,
 	height: 600
 };
-// 시야각 = fov 망원경 vs 전체 , aspect Ratio  width / height of viewPort
+
 const camera = new THREE.PerspectiveCamera(60, sizes.width / sizes.height);
 camera.position.set(1, 1, 10);
-
-//target is Vector3
-camera.lookAt(mesh.position); // will look at the center of mesh
 
 scene.add(camera);
 
 //renderer
-//render scene from the camera point
+
 const canvas = document.querySelector('.webgl');
 const renderer = new THREE.WebGLRenderer({
 	canvas: canvas
 });
-
-// renderer의 사이즈를 정함
 
 renderer.setSize(sizes.width, sizes.height);
 
