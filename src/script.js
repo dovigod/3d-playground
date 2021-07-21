278e3e62996a4a51bd64e6a351cc81214cb47015
서지상 <inaki96@naver.com>
2021-07-22 00:11:41 +0900

    3d text , performance underConsideration

--- src/script.js
+++ src/script.js
@@ -4,44 +4,47 @@ import gsap from 'gsap';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import * as dat from 'dat.gui';
 
+//fonts
+
+const fontLoader = new THREE.FontLoader();
+//use callback in param
+fontLoader.load('/fonts/helvetiker_regular.typeface.json' , (font) =>{
+	const textGeometry = new THREE.TextBufferGeometry(
+		'Hello From Jisang',
+		{
+			font,
+			size: 0.5,
+			height:0.2, //depth of font
+			curveSegments: 12,
+			bevelEnabled: true,
+			bevelThickness : 0.03,
+			bevelSize: 0.02,
+			bevelOffset: 0,
+			bevelSegments: 5
+
+		}
+	)
+	//bevel => round the font , text
+
+	const textMaterial = new THREE.MeshBasicMaterial()
+	const text = new THREE.Mesh(textGeometry, textMaterial)
+	gui.add(textMaterial , 'wireframe')
+	scene.add(text);
+	console.log('font loaded!')
+})
 
 //textures 
-const loader = new THREE.TextureLoader()
-
-const colorTexture = loader.load('/textures/door/color.jpg');
-const alphaTexture = loader.load('/textures/door/alpha.jpg');
-const heightTexture = loader.load('/textures/door/height.jpg');
-const normalTexture = loader.load('/textures/door/normal.jpg');
-const ambientOcclusionTexture = loader.load('/textures/door/ambientOcclusion.jpg');
-const metalnessTexture = loader.load('/textures/door/metalness.jpg');
-const roughnessTexture = loader.load('/textures/door/roughness.jpg');
-const gradTexture = loader.load('/gradients/3.jpg')
-const matTexture = loader.load('/matcaps/8.png')
+const textureLoader = new THREE.TextureLoader()
 
 //for enviornment mapping
 
 const cubeLoader = new THREE.CubeTextureLoader();
-
-const enviornmentTexture = cubeLoader.load([
-'/environmentMaps/htrTexture/px.png',
-'/environmentMaps/htrTexture/nx.png',
-'/environmentMaps/htrTexture/py.png',
-'/environmentMaps/htrTexture/ny.png',
-'/environmentMaps/htrTexture/pz.png',
-'/environmentMaps/htrTexture/nz.png'])
-
-//need 6 parameter for up down right left forward back
 
 const gui = new dat.GUI();
 gui.hide();
  
 const meshParams = {
 	color: '#ffffff',
-	spin: () => {
-		gsap.to(plane.rotation, { duration: 1, y: plane.rotation.y + Math.PI * 2 });
-		gsap.to(sphere.rotation, { duration: 1, y: sphere.rotation.y + Math.PI * 2 });
-		gsap.to(torus.rotation, { duration: 1, y: torus.rotation.y + Math.PI * 2 });
-	}
 };
 const sizes = {
 	width: window.innerWidth,
@@ -50,71 +53,8 @@ const sizes = {
 
 const scene = new THREE.Scene();
 
-//object
 
 
-const material = new THREE.MeshStandardMaterial();
-
-material.metalness = 0.7;
-material.roughness = 0.2;
-material.envMap = enviornmentTexture
-// env reflection on material can be seen
-
-
-//three.js only supports cube map
-
-
-
-gui.add(material , 'wireframe')
-
-
-
-
-const sphere = new THREE.Mesh(
-	new THREE.SphereGeometry(.5,16,16), material, 64 ,64);
-
-	sphere.geometry.setAttribute('uv2' , new THREE.BufferAttribute(sphere.geometry.attributes.uv.array, 2));
-
-
-const plane = new THREE.Mesh(
-	new THREE.PlaneGeometry(1,1 ,100,100) , material
-)
-
-plane.geometry.setAttribute('uv2' , new THREE.BufferAttribute(plane.geometry.attributes.uv.array, 2));
-
-
-
-const torus = new THREE.Mesh(
-	new THREE.TorusGeometry(.5,.2,64,128),
-	material
-)
-
-torus.geometry.setAttribute('uv2' , new THREE.BufferAttribute(torus.geometry.attributes.uv.array, 2));
-
-
-sphere.geometry.setAttribute('uv2' , new THREE.BufferAttribute(sphere.geometry.attributes.uv.array, 2));
-
-
-sphere.position.x = -1.5
-torus.position.x = 1.5
-
-scene.add(sphere , plane , torus);
-
-
-gui.add(meshParams , 'spin')
-
-
-
-//lights
-
-const ambientLight = new THREE.AmbientLight(0xffffff , 0.5)
-scene.add(ambientLight);
-
-const pointLight = new THREE.PointLight(0xffffff, 0.5)
-pointLight.position.x = 2
-pointLight.position.y = 3
-pointLight.position.z = 4
-scene.add(pointLight)
 
 const camera = new THREE.PerspectiveCamera(60, sizes.width / sizes.height, 0.1, 100);
 camera.position.set(1, 1, 2);
@@ -130,20 +70,6 @@ const renderer = new THREE.WebGLRenderer({
 
 renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-
-
-//debug
-
-const materialFolder = gui.addFolder('material')
-materialFolder.add(material , 'metalness', 0 , 1 , 0.01)
-materialFolder.add(material , 'roughness', 0 , 1 , 0.01)
-materialFolder.addColor(meshParams, 'color').onChange( () =>{
-	material.color.set(meshParams.color)
-})
-materialFolder.add(material ,'transparent')
-materialFolder.add(material , 'opacity' , 0 , 1 , 0.01)
-materialFolder.add(material , 'wireframe');
-materialFolder.add(material , 'flatShading')
 
 
 
