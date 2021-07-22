04e9ebc314e0839235fac38be9b2dfc6b32bd4ac
서지상 <inaki96@naver.com>
2021-07-22 16:34:46 +0900

    light starting

--- src/script.js
+++ src/script.js
@@ -6,118 +6,53 @@ import * as dat from 'dat.gui';
 
 const scene = new THREE.Scene();
 
-//textures 
-const textureLoader = new THREE.TextureLoader()
-
-const matcapTexture = textureLoader.load('/matcaps/8.png')
-//fonts
-
 const fontLoader = new THREE.FontLoader();
-//use callback in param
-
-const material = new THREE.MeshMatcapMaterial({
-	matcap : matcapTexture
-})
-fontLoader.load('/fonts/helvetiker_regular.typeface.json' , (font) =>{
-	const textGeometry = new THREE.TextBufferGeometry(
-		'Hello From Jisang',
-		{
-			font,
-			size: 0.5,
-			height:0.2, //depth of font
-			curveSegments: 5,
-			bevelEnabled: true,
-			bevelThickness : 0.001, // 뽈록이, 많을수록 튀어나와
-			bevelSize: 0.02, 
-			bevelOffset: 0,
-			bevelSegments: 1,	 // 테두리.. 많을수록 둥글어
-		}
-	)
-	// going to use bouning for frustum culling to let three.js caclulate geometries easily
-	textGeometry.computeBoundingBox()
-	//make Box3-> coordinates of box ,  
-
-	textGeometry.translate(
-		-((textGeometry.boundingBox.max.x-0.02) * 0.5), // bevel size
-		-((textGeometry.boundingBox.max.y-0.02) * 0.5), // bevelsize
-		-((textGeometry.boundingBox.max.z-0.03 )* 0.5)  //bevelthickness
-	)
-
-	textGeometry.computeBoundingBox()
-	//re computejust use below!!!!!!
-
-	// textGeometry.center()
-
-	//actually , 
-	
-	const text = new THREE.Mesh(textGeometry, material)
-	
-	scene.add(text);
-	console.log('font loaded!')
-
-	
 
 
-	
-})
-//for enviornment mapping
+//object
 
-const cubeLoader = new THREE.CubeTextureLoader();
+const material = new THREE.MeshStandardMaterial({color:0xffffff});
+material.roughness = 0.4
 
-const gui = new dat.GUI();
-gui.hide();
- 
-const meshParams = {
-	color: '#ffffff',
-};
+const plane = new THREE.Mesh(new THREE.PlaneGeometry(3,3), material);
+plane.rotation.x= -Math.PI * 0.5
+plane.position.y = -1
+
+const sphere = new THREE.Mesh(new THREE.SphereGeometry(.5,16,16) , material)
+sphere.position.set(-1.5,0,0);
+
+const cube = new THREE.Mesh(new THREE.BoxGeometry(1,1,1) , material)
+
+const torus = new THREE.Mesh(
+    new THREE.TorusGeometry(0.3, 0.2, 32, 64),
+    material
+)
+torus.position.x = 1.5
+scene.add(plane);
+scene.add(sphere);
+scene.add(cube);
+scene.add(torus);
+
+
+//light
+const ambientLight = new THREE.AmbientLight('#ffffff', 0.5)
+const pointLight = new THREE.PointLight('#ffffff',0.5)
+pointLight.position.x = 2
+pointLight.position.y = 3
+pointLight.position.z = 4
+
+scene.add(ambientLight)
+scene.add(pointLight)
+
+
+
 const sizes = {
 	width: window.innerWidth,
 	height: window.innerHeight
-};
-
-
-
-//donuts100 ; 
-
-//method 1
-
-console.time('donut')
-const donutGeometry = new THREE.TorusBufferGeometry(0.3, 0.2 , 20 , 45);
-	
-for(let i = 0 ; i < 100 ; i ++){
-	
-	const donut = new THREE.Mesh(donutGeometry , material)
-
-	donut.position.x = (Math.random() -0.5 )* 100 // normalize value !! usef!!
-	donut.position.y = (Math.random() -0.5 )* 100
-	donut.position.z = (Math.random() -0.5 )* 100
-
-
-	donut.rotation.x = Math.random() * Math.PI
-	donut.rotation.y = Math.random() * Math.PI
-	donut.rotation.z = Math.random() * Math.PI
-
-	const scale = (Math.random()+1)*4
-
-	donut.scale.x =scale;
-	donut.scale.y =scale;
-	donut.scale.z =scale;
-
-	scene.add(donut)
-
-	
 }
-console.timeEnd('donut')
-// 76ms ..  -> 322ms , 
-
-//use meshes , when making same multiple mesh , 
-//-> to 5ms
-
-
 
 const camera = new THREE.PerspectiveCamera(60, sizes.width / sizes.height, 0.1, 100);
 camera.position.set(3, 3, 3);
-
 scene.add(camera);
 
 //renderer
@@ -140,13 +75,25 @@ controls.target.y = 1;
 controls.update();
 
 const clock = new THREE.Clock();
-
-
 const animate = () => {
+
+	const elapsedTime = clock.getElapsedTime()
+
+	cube.rotation.x = elapsedTime * Math.PI * 0.2
+	cube.rotation.y = elapsedTime * Math.PI * 0.4
+	cube.rotation.z = elapsedTime * Math.PI * 0.6
+
+	torus.rotation.x = elapsedTime * Math.PI * 0.2
+	torus.rotation.y = elapsedTime * Math.PI * 0.4
+	torus.rotation.z = elapsedTime * Math.PI * 0.6
+
+
 	controls.update(); // for damping
 	renderer.render(scene, camera);
 	window.requestAnimationFrame(animate);
 };
+
+
 
 const eventListeners = () => {
 	window.addEventListener('resize', (e) => {
@@ -156,6 +103,8 @@ const eventListeners = () => {
 		camera.updateProjectionMatrix();
 		renderer.setSize(sizes.width, sizes.height);
 	});
+
+
 
 window.addEventListener('dblclick', (e) => {
 		const fullscreenElement = document.fullscreenElement || document.webkitFullscreenElement;
