aaab8c5137d9a1bf45fdea6aaa7ec47cf95f2118
서지상 <inaki96@naver.com>
2021-08-05 18:05:32 +0900

    ray casters

--- src/script.js
+++ src/script.js
@@ -7,9 +7,7 @@ import * as dat from 'dat.gui'
  * Base
  */
 // Debug
-const gui = new dat.GUI({
-	width: 360
-})
+const gui = new dat.GUI()
 
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
@@ -17,84 +15,33 @@ const canvas = document.querySelector('canvas.webgl')
 // Scene
 const scene = new THREE.Scene()
 
-//galaxy
-const parameters = {
-	count : 1000,
-	size : 0.01,
-	radius: 5,
-	branch: 3,
-	spin: 1,
-	randomness : 0.2,
-	randomnessPow : 3,
-	insideColor: '#ff5588',
-	outsideColor : '#8855ff'
-}
-let particlesGeometry = null
-let particlesMaterial = null
-let points = null
-const generateGalaxy = () => {
+/**
+ * Objects
+ */
+const object1 = new THREE.Mesh(
+    new THREE.SphereGeometry(0.5, 16, 16),
+    new THREE.MeshBasicMaterial({ color: '#ff0000' })
+)
+object1.position.x = - 2
 
-	if(points !== null){
-		particlesGeometry.dispose()
-		particlesMaterial.dispose()
-		scene.remove(points)
-	}
+const object2 = new THREE.Mesh(
+    new THREE.SphereGeometry(0.5, 16, 16),
+    new THREE.MeshBasicMaterial({ color: '#ff0000' })
+)
 
-	particlesGeometry = new THREE.BufferGeometry()
-	const positions = new Float32Array(parameters.count * 3)
-	const colors = new Float32Array(parameters.count * 3)
+const object3 = new THREE.Mesh(
+    new THREE.SphereGeometry(0.5, 16, 16),
+    new THREE.MeshBasicMaterial({ color: '#ff0000' })
+)
+object3.position.x = 2
 
-	//base color
+scene.add(object1,object2,object3)
 
-	const colorInside = new THREE.Color(parameters.insideColor);
-	const colorOutside = new THREE.Color(parameters.outsideColor);
+/**
+ * raycasters
+ */
 
-
-	for( let i = 0 ; i < parameters.count ; i ++){
-		const i3 = i*3
-
-		//positions
-		const radius = (Math.random()) * parameters.radius
-		const branchesAngle = ((i % parameters.branch) / parameters.branch) * Math.PI * 2
-		const spinAngle = radius * parameters.spin
-
-		const randomX = Math.pow(( Math.random()) , parameters.randomnessPow)  * (Math.random() < 0.5 ? 1 : - 1)
-		const randomY = Math.pow(( Math.random()) , parameters.randomnessPow) * (Math.random() < 0.5 ? 1 : - 1)
-		const randomZ = Math.pow(( Math.random()) , parameters.randomnessPow) * (Math.random() < 0.5 ? 1 : - 1)
-
-		positions[i3] = radius * Math.cos(branchesAngle + spinAngle) + randomX 
-		positions[i3+1] = randomY
-		positions[i3+2] = radius * Math.sin(branchesAngle + spinAngle) + randomZ
-
-		//color
-		const mixedColor = colorInside.clone()
-		mixedColor.lerp(colorOutside , radius / parameters.radius)
-		colors[i3 + 0] = mixedColor.r
-		colors[i3 + 1] = mixedColor.g
-		colors[i3 + 2] = mixedColor.b
-
-	}
-	particlesGeometry.setAttribute('color', new THREE.BufferAttribute(colors, 3))
-	particlesGeometry.setAttribute('position', new THREE.BufferAttribute(positions, 3))
-
-	
-	//lerp
-
-	colorInside.lerp(colorOutside , 0.5)
-
-	// material
-	particlesMaterial = new THREE.PointsMaterial({
-		size: parameters.size,
-		sizeAttenuation: true,
-		depthWrite: false,
-		blending: THREE.AdditiveBlending,
-		vertexColors: true
-	})
-	points = new THREE.Points(particlesGeometry , particlesMaterial)
-	scene.add(points)
-
-
-}
+ const raycaster = new THREE.Raycaster()
 
 /**
  * Sizes
@@ -124,8 +71,6 @@ window.addEventListener('resize', () =>
  */
 // Base camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.x = 3
-camera.position.y = 3
 camera.position.z = 3
 scene.add(camera)
 
@@ -141,46 +86,49 @@ const renderer = new THREE.WebGLRenderer({
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-//dat.gui
-gui.add(parameters,'count' , 100 , 100000 , 10).onFinishChange(( )=> {
-	generateGalaxy()
-})
-gui.add(parameters ,'size' , 0.001 , 0.1 , 0.001).onFinishChange(() => {
-	generateGalaxy()
-})
-gui.add(parameters ,'radius' , 0.01 , 20 , 0.01).onFinishChange(() => {
-	generateGalaxy()
-})
-
-gui.add(parameters ,'branch' , 3 , 10 , 1).onFinishChange(() => {
-	generateGalaxy()
-})
-
-gui.add(parameters ,'spin' , -5 , 5 , 1).onFinishChange(() => {
-	generateGalaxy()
-})
-
-gui.add(parameters ,'randomness' , 0, 20 , 0.01).onFinishChange(() => {
-	generateGalaxy()
-})
-gui.add(parameters ,'randomnessPow' , 1, 10 , 0.001).onFinishChange(() => {
-	generateGalaxy()
-})
-gui.addColor(parameters, 'insideColor')
-gui.addColor(parameters, 'outsideColor')
-
-//problem , we are generating new galaxy, but not destroying old ones
 
 /**
  * Animate
  */
 const clock = new THREE.Clock()
 
+let currentIntersect = null
+
+//mouse
+const mouse = new THREE.Vector2()
+window.addEventListener('mousemove' , (e) =>{
+	mouse.x = e.clientX / sizes.width * 2 -1 
+	mouse.y = -(e.clientY / sizes.height) * 2 + 1
+})
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
 
-    // Update controls
+	//animate
+	object1.position.y = Math.sin(elapsedTime * 0.3) * 1.5
+	object2.position.y = Math.sin(elapsedTime * 0.6) * 1.5
+	object3.position.y = Math.sin(elapsedTime * 0.4) * 1.5
+
+	//raycaster
+
+	raycaster.setFromCamera(mouse, camera)
+	// const rayOrigin = new THREE.Vector3(-3,0,0)
+	// const rayDirection = new THREE.Vector3(10,0,0) //should be normalized
+	// rayDirection.normalize()
+	// raycaster.set(rayOrigin, rayDirection)
+	const testing = [object1,object2,object3]
+    const intersects = raycaster.intersectObjects(testing) // use 's' even if u use single obj
+	
+	for(const object of testing){
+		object.material.color = new THREE.Color('#ff0000')
+	}
+	
+	for(const intersect of intersects){
+		intersect.object.material.color = new THREE.Color('#0000ff')
+	}
+	
+	
+	// Update controls
     controls.update()
 
     // Render
@@ -189,7 +137,4 @@ const tick = () =>
     // Call tick again on the next frame
     window.requestAnimationFrame(tick)
 }
-
 tick()
-
-generateGalaxy()
